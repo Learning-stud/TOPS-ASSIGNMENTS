df8eb88b9fdd323f85d513436427913d7faf9957
Learning-stud <jayahalpara123456@gmail.com>
2023-12-05 03:37:00 +0530

    ,.,.,.

---
+++ REACT-REDUX/searching-table/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import TableComponent from './components/TableComponent';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <TableComponent/>
+  </React.StrictMode>
+);

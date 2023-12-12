66b958f91025dab99c980debbc6a98bc2c61ffa6
Learning-stud <jayahalpara123456@gmail.com>
2023-12-12 17:37:41 +0530

    MODULE=> 3 => Q-2

---
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-2.py
@@ -0,0 +1,8 @@
+#Q-2 how to remove last object from list
+
+lists = ["jay ","abhishek" ,"devanshu","nehru","ganddhi"]
+
+#  by given the index number of the object we can remove the object from list and if we want to remove the whole object in list we have to just use inbuilt pop function
+
+lists.pop(4)
+print(lists)

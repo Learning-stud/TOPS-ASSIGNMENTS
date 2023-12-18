ff336eea5540921d9d5fc14df2426cd451c1a2d2
Learning-stud <jayahalpara123456@gmail.com>
2023-12-18 17:07:00 +0530

    Q-4 Answer

--- PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-4.py
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-4.py
@@ -1,6 +1,7 @@
 # write a function to find smallest ,largest and sum of all from the list
 # The code `num = []` initializes an empty list called `num`.
 num = []
+add = 0
 for i in range(5):
  # The code `num_to_take = int(input("Enter the Number: => :"))` is taking user input and converting
  # it to an integer. It prompts the user to enter a number and assigns that number to the variable
@@ -13,6 +14,8 @@ for i in range(5):
 num.sort()
 smallest = num[0]
 largest = num[-1]
+add = smallest + largest
 print("Sorted List :  ", num)
 print("Smallest Number :  ", smallest)
 print("largest Number :  ", largest)
+print("Sum Of All Number :  ", add)

4e983ca136f7c618192e8183194f3f0732da1f2f
Learning-stud <jayahalpara123456@gmail.com>
2023-12-19 15:24:02 +0530

    Q-6 Answer

---
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-6.py
@@ -0,0 +1,12 @@
+def count_strings(strings):
+ count= 0
+ for counts in strings:
+  if len(counts) >= 2 and counts[0]==counts[-1]:
+     counts += 1
+  return count
+
+
+counted_strings = ["Level","Hello","Devanshu","tirth","Abhishek"]
+result_counts = count_strings(counted_strings)
+
+print(f"Nu,ber Of Strings With The  Same First And Last Characte: {result_counts}")

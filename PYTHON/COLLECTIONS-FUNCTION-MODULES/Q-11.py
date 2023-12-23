670f8ce925dfbb3e5f27401e3fa2386d74e39e98
Learning-stud <jayahalpara123456@gmail.com>
2023-12-24 01:08:19 +0530

    Q-11 Answer

---
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-11.py
@@ -0,0 +1,12 @@
+#Write a Python function that takes a list and returns a new list with unique
+# elements of the first list.
+
+def get_unique_elements(input_list):
+    return list(set(input_list))
+
+# Take input as a string and convert it to a list
+original_input = input("Enter Values separated by spaces: ")
+original_list = original_input.split()
+
+unique_elements = get_unique_elements(original_list)
+print("List with Unique Elements: ", unique_elements)

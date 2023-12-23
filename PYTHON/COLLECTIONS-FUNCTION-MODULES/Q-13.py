6c2262e0926a44a9bf442ca451a278cba4e4bb03
Learning-stud <jayahalpara123456@gmail.com>
2023-12-24 01:37:55 +0530

    Q-13 Answer

---
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-13.py
@@ -0,0 +1,5 @@
+# Write a Python program to select an item randomly from a list
+import random
+my_list= [1,2,3,4,5]
+random_items = random.choice(my_list)
+print("Randomly selected items : ",random_items)

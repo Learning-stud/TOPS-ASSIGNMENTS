a57461194dadaa85367cbe019b68c4ef74c7c173
Learning-stud <jayahalpara123456@gmail.com>
2023-12-24 01:41:05 +0530

    Q-14 Answer

---
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-14.py
@@ -0,0 +1,6 @@
+# Find the Second Smallesst Number IN A list
+
+
+number = [5,1,21,14,15,54,3]
+smallest = sorted(set(number))[1]  # by the hep of sorted and set method and giving the index number
+print("Second Smallest Number is :",smallest)

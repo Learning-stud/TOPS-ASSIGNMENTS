3fb4129be23b811547ffbdeaa9ec641e3c2e609f
Learning-stud <jayahalpara123456@gmail.com>
2023-12-27 06:25:26 +0530

    Q-10 Answer

--- PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-10.py
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-10.py
@@ -1,6 +1,6 @@
-square = [ i ** 2 for i in range (1,31)]
+square = [ i ** 2 for i in range (1,31)]    # i ** 2  is used to find the cube root
 print("The First Elemnts")
-print(square[:5])
+print(square[:5]) # [:5]   is used to print the values from positive index
 
 print("The last Elements ")
-print(square[:-5])
+print(square[:-5])  # [:-5 is used to print the values from negative index  {: this sign is used to reversed  }]

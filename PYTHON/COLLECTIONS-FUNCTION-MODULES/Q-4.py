d5ce7c132fdabfc6cd935172de122ee8e09c3adf
Learning-stud <jayahalpara123456@gmail.com>
2023-12-27 06:56:16 +0530

    Detai Added

--- PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-4.py
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-4.py
@@ -3,18 +3,16 @@
 num = []  # initializing
 add = 0  # assigning  default value
 for i in range(5):  # defining the range
- # The code `num_to_take = int(input("Enter the Number: => :"))` is taking user input and converting
- # it to an integer. It prompts the user to enter a number and assigns that number to the variable
-# `num_to_take`.
+# `num_to_take`.  
  num_to_take = int(input("Enter the Number: => :"))
  num.append(num_to_take)   # adding the element
 
 # The code `num.sort()` is sorting the list `num` in ascending order.
 
 num.sort()
-smallest = num[0]
-largest = num[-1]
-add = smallest + largest
+smallest = num[0]  # First element
+largest = num[-1] # Last Elements
+add = smallest + largest  #
 print("Sorted List :  ", num)
 print("Smallest Number :  ", smallest)
 print("largest Number :  ", largest)

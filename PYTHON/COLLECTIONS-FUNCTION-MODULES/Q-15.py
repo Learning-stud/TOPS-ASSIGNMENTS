c6940bd9a0ea596994a0cc470b39b6324e03fe92
Learning-stud <jayahalpara123456@gmail.com>
2023-12-27 06:25:53 +0530

    Q-15 Answer

---
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-15.py
@@ -0,0 +1,6 @@
+# Write a Python program to get unique values from a list
+
+# first taking and list
+orignal = [1,2,2,3,4,4,5,6,6]  # list will be checked
+unique_list = list(set(orignal)) # r=then by the sort method duplicates will be removed
+print("Unique Values : ",unique_list)

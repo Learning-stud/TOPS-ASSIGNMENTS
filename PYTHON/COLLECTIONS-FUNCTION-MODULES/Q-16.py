acde82cf4ba13f1fa96b63f6f295fe001a03b1ce
Learning-stud <jayahalpara123456@gmail.com>
2023-12-31 07:12:28 +0530

    Q-16 Answer

--- PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-16.py
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-16.py
@@ -1,3 +1,9 @@
 # Write a Python program to check whether a list contains a sub list
 
-orignal_list = [1,2,3,[4,5],6]
+
+def is_sublist(lst,sub_list):
+ return all(x in lst for x in sub_list)
+
+my_list = [1,2,3,[1,2,3,],4,5]
+sub_list = [1,2,3]
+print(is_sublist(my_list,sub_list))

7e3a856d6fe5c2a804271eee9eae6cfe89188535
Learning-stud <jayahalpara123456@gmail.com>
2023-12-31 08:31:51 +0530

    Q-18 Answer

---
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-18.py
@@ -0,0 +1,10 @@
+# what is tuple ,diffrence between tuple and list
+
+# tuple is an immutable data type in python#
+#Tuple cannot be modified
+
+
+
+
+#list is mutable data type in python
+# list can be modified

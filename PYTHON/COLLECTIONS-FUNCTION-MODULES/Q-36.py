5f3ec1416f13ddd016e61d8842c17c7138fea38a
Learning-stud <jayahalpara123456@gmail.com>
2024-01-01 13:05:59 +0530

    Q-36 Answer

---
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-36.py
@@ -0,0 +1,12 @@
+# Write a Python script to merge two Python dictionaries
+
+
+dictionary1= {'1':'jay','2':'is','3':'going'}
+dictionary2={'4':'to','5':'complete','6':'assignment'}
+
+
+# update method
+merged = dictionary1.copy()
+merged.update(dictionary2)
+
+print("Merged Dictionary : ",merged)

8411b04b3aa7a97b534fcf7138d3258fa670f8db
Learning-stud <jayahalpara123456@gmail.com>
2024-01-01 11:56:04 +0530

    Q-1 Answer

---
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-1.py
@@ -0,0 +1,15 @@
+ # A list is a versatile and mutable data structure that allows you to store a collection of items.
+
+#Lists are defined by enclosing elements in square brackets [] and separating them with commas.
+
+#Lists  contain elements of different data types, and we can perform various operations like adding, removing, and accessing elements in a list.
+
+
+# by using reverse method []
+
+
+
+my_list=[1,2,3,4,5]
+
+my_list.reverse()
+print("Reversed List",my_list)

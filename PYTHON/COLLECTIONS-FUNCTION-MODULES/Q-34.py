8af9e9fb4aba7245d492841e3546c3c9de8fb7d4
Learning-stud <jayahalpara123456@gmail.com>
2024-01-01 11:39:29 +0530

    Q-34 Answer

---
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-34.py
@@ -0,0 +1,9 @@
+# How Do You Traverse Through A Dictionary Object In Python?
+
+
+my_dictionary = {'j':'jay','d':'devanshu','t':'Tirth'}
+
+for key, value in my_dictionary.items():
+ print(f"Key : {key}, Value : {value}")
+
+ 

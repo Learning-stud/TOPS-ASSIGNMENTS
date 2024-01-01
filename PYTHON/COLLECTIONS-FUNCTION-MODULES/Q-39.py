a618a2bb8b6704319829019483c71a48cd211fa3
Learning-stud <jayahalpara123456@gmail.com>
2024-01-01 13:07:13 +0530

    Q-39 Answer

---
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-39.py
@@ -0,0 +1,8 @@
+#unique values in dictionary
+
+my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 2, 'e': 1}
+
+#  using set
+unique_values = set(my_dict.values())
+
+print("Unique Values in the Dictionary:", unique_values)

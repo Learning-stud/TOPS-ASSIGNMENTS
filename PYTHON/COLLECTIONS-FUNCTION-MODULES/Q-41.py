f928d85d4b4b02dd4b12f577de50b7351a0354cb
Learning-stud <jayahalpara123456@gmail.com>
2024-01-02 09:46:50 +0530

    Q-41 Answer

--- PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-41.py
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-41.py
@@ -6,9 +6,9 @@ combinations = {'1': ['j', 'a', 'y'], '2': ['d', 'e', 'v', 'a', 'n', 's', 'h', '
 
 # nested loops to generate all combinations
 combinations_add = []
-for char1 in combinations['1']:
-    for char2 in combinations['2']:
-        combinations_add.append(char1 + char2)
+for char1 in combinations['1']: # loop for first value
+    for char2 in combinations['2']: # loop for second value
+        combinations_add.append(char1 + char2) # using append to add the value  and after the vlue will be added then concetination of every posible combination
 
 print("All Combinations of Letters:", combinations_add)
 

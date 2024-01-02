ab3dae7e2bb873eb7810a9e916232d23918009b8
Learning-stud <jayahalpara123456@gmail.com>
2024-01-02 09:44:19 +0530

    Q-10 Answer

--- PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-10.py
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-10.py
@@ -1,3 +1,5 @@
+#Write a Python program to generate and print a list of the first and last 5 elements where the values are square of numbers between 1 and 30.
+
 square = [ i ** 2 for i in range (1,31)]    # i ** 2  is used to find the cube root
 print("The First Elemnts")
 print(square[:5]) # [:5]   is used to print the values from positive index

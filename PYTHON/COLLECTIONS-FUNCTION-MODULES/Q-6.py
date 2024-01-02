02b62f19f101f1d6e7d67c4a24ca6e96e1d644f7
Learning-stud <jayahalpara123456@gmail.com>
2024-01-02 08:40:16 +0530

    Q-6 Answer

--- PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-6.py
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-6.py
@@ -1,3 +1,5 @@
+# Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are the same from a given list of strings.
+
 def count_strings(strings):
  count= 0
  for counts in strings:

8c5c4ceaf179dfc6370f758324881d9dad3c344b
Learning-stud <jayahalpara123456@gmail.com>
2024-01-05 13:59:47 +0530

    .

--- PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-15.py
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-15.py
@@ -2,5 +2,5 @@
 
 # first taking and list
 orignal = [1,2,2,3,4,4,5,6,6]  # list will be checked
-unique_list = list(set(orignal)) # r=then by the sort method duplicates will be removed
+unique_list = list(set(orignal)) # by sort method duplicates will be removed
 print("Unique Values : ",unique_list)
--- PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-26.py
+++ PYTHON/COLLECTIONS-FUNCTION-MODULES/Q-26.py
@@ -1,5 +1,5 @@
 # replace last value of tuple in a list
 list_of_tuples = [(1, 2), (3, 4), (5, 6)]
 new_value = 10
-modified_list = [t[:-1] + (new_value,) for t in list_of_tuples]
+modified_list = [t[:-1] + (new_value,) for t in list_of_tuples] #  [:-1] except the last value nothing will be changed  thn append the value [+ (new_value)]
 print(modified_list)
